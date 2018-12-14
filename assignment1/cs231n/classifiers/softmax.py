a8987fba8cec12ae3f2325c36ec2a8b8df514ae5
dipeshtamboli <dipeshtamboli@gmail.com>
2018-12-14 14:25:21 +0530

    softmax completed

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -36,24 +36,32 @@ def softmax_loss_naive(W, X, y, reg):
   #code
   #####
   
-  score=X.dot(W)
-  e_score=np.exp(score)
-  e_score_sum=np.sum(e_score,axis=1)
-  e_score_sum=e_score_sum.reshape(len(e_score_sum),1)
-  e_score_norm=np.divide(e_score,e_score_sum)
-  predicted=e_score_norm[np.arange(num_train),y]
-  predicted_log=-np.log(predicted)
-  loss=np.sum(predicted_log,axis=0)
+  for i in range(num_train):
+    # cal_score=X[i].dot(W)
+    # print(X.shape)
+    # print(W.shape)
+    # print(cal_score.shape)
+    # cal_score_norm=np.divide(cal_score, np.sum(cal_score,axis=0))
+    # print(cal_score_norm.shape)
+    # predicted=cal_score_norm[i,y[i]]
+    # predicted_log=-np.log(predicted)
+    # loss+=predicted_log
+  
+    score=X[i].dot(W)
+    score-=np.max(score)
+    sum_row=np.sum(np.exp(score))
+    norm = lambda k: np.exp(score[k])/sum_row
+    loss +=-np.log(norm(y[i]))
+
+    for j in range(num_class):
+       norm_j=norm(j)
+       dW[:,j]+=(norm_j - (j==y[i]))*X[i]
+
+  dW /=num_train
+  dW+=reg*np.sum(W*W)
   loss/=num_train
   loss+=0.5*reg*np.sum(W*W)
-  
-  # for i in range(num_train):
-  #   for j in range(num_class):
-  #     cal_score=x[i].dot(W)
-  #     cal_score_norm=np.divide(cal_score, np.sum(cal_score,axis=1))
-  #     predicted=cal_score_norm[i,y[j]]
-  #     predicted_log=-np.log(predicted)
-  #     loss+=predicted_log
+
   #####
   pass
   #############################################################################
@@ -72,13 +80,36 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  num_train=X.shape[0]
+  num_class=W.shape[1]
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+  ##########
+  #CODE
+  #########
+  score=X.dot(W)
+  score=score-np.max(score,axis=1,keepdims=True)
+  e_score=np.exp(score)
+  e_score_sum=np.sum(e_score,axis=1)
+  e_score_sum=e_score_sum.reshape(len(e_score_sum),1)
+  e_score_norm=np.divide(e_score,e_score_sum)
+  predicted=e_score_norm[np.arange(num_train),y]
+  predicted_log=-np.log(predicted)
+  loss=np.sum(predicted_log,axis=0)
+  loss/=num_train
+  loss+=0.5*reg*np.sum(W*W)
+
+  mat_correct_scores=np.zeros_like(e_score_norm)
+  mat_correct_scores[np.arange(num_train),y]=1
+  dloss_by_dscore=e_score_norm- mat_correct_scores
+  dW=X.T.dot(dloss_by_dscore)
+  dW/=num_train
+  dW+=reg*W
+  #########
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #
